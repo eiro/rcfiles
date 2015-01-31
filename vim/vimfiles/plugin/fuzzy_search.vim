eiro/rcfiles

commit e856ae6fc4b1dd1baec98c6d1ea50c0aa9228e31

fuzzy search is ignorecase and verymagic
as expected ...

vim/vimfiles/plugin/fuzzy_search.vim
@@ -26,7 +26,7 @@ fun FuzzySearch(...)
     " by adding non greedy matches (:h /\{)
     " between the parts ot the fuzzy. 
 
-    let @/ = join(a:000,'.\{-}') 
+    let @/ = '\c\v'.join(a:000,'.\{-}') 
 
     " add it in the search history
     call histadd('/',@/)

commit 63b9afff3d9a6d4ebdb60183a34dc864dee17908

bugfix with verymagic
fix bug introduced in e856ae6fc4b1dd1baec98c6d1ea50c0aa9228e31.
the fuzzy search wasn't working anymore.

vim/vimfiles/plugin/fuzzy_search.vim
@@ -26,7 +26,7 @@ fun FuzzySearch(...)
     " by adding non greedy matches (:h /\{)
     " between the parts ot the fuzzy. 
 
-    let @/ = '\c\v'.join(a:000,'.\{-}') 
+    let @/ = '\c\v'.join(a:000,'.{-}') 
 
     " add it in the search history
     call histadd('/',@/)