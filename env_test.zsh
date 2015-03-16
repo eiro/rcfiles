a89b4cf5843f6c0d268c1c7a93cecc0ce7b62825
Marc Chantreux <khatar@phear.org>
2015-03-16 14:30:14 +0100

    test if the huffman army is here
    huffman army is the set of my very short aliases i use almost all the
    time. now i want them to be there.

--- env_test.zsh
+++ env_test.zsh
@@ -4,12 +4,24 @@ lf=ldap/filter
 
 my_env_works () {
     local got expected
+
     got=$( $lf/minify <<< "
         (| (uid=a)
            (uid=b))" )
     expected="(|(uid=a)(uid=b))"
-    is $got $expected "ldap minification works" || note "got $got"
+    is $got $expected "ldap minification works" ||
+        note "got $got"
 
-}
+    local x
 
-prove my_env_works 
+    # based on Huffman principle, those are the commands 
+    # i need to survive (i use them in vim also)
+
+    for x ( E s z sz g rt t lfm ) {
+        shush whence $x; ok "$x is ready for action"
+    }
+
+} 
+
+
+prove my_env_works   
