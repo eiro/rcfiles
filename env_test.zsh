07cdeaf4d767441ae7b82e66e9cbfa88b72980b9
Marc Chantreux <khatar@phear.org>
2015-03-30 18:21:49 +0200

    ldap filter test suite added
    test suite run ok

--- env_test.zsh
+++ env_test.zsh
@@ -1,16 +1,6 @@
 uzu devel/TAP :all
-uzu ldap
-lf=ldap/filter
 
 my_env_works () {
-    local got expected
-
-    got=$( $lf/minify <<< "
-        (| (uid=a)
-           (uid=b))" )
-    expected="(|(uid=a)(uid=b))"
-    is $got $expected "ldap minification works" ||
-        note "got $got"
 
     local x
 
