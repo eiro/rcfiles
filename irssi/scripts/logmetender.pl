16ec511297bc566e0db0af84dba2d50471e6af7b
Marc Chantreux <khatar@phear.org>
2015-06-18 16:57:17 +0200

    added minimal documentation about using logmetender

--- irssi/scripts/logmetender.pl
+++ irssi/scripts/logmetender.pl
@@ -3,6 +3,27 @@ use Eirotic;
 our ($VERSION, %IRSSI);
 use re '/x';
 
+=head1 SYNOPSIS
+
+logmetender is a minimalistic notification system. 
+
+from your remote machine, you can 
+
+    n/irssi/logmetender () {
+        local srv=${1?the server hosting your irssi client}
+        local log=${2:-/tmp/irssi.notify}
+        ssh $srv tail -fn0 $log | xargs -n1 -d$'\n' notify-send
+    }
+
+    n/irssi/logmetender shells.example.com  &
+
+you must clean the log by yourself (daily cron job?) 
+
+    ssh shells.example.com ':>/tmp/irssi.notify'
+
+=cut
+
+
 sub _log { 
 
     state $fh = do {
